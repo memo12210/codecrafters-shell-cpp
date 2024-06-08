4fe261148aa70bea725aa20ca85c23858e370268
Mehmet <mehmetcalikoglufb@gmail.com>
2024-06-08 16:06:31 +0300

    1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+  std::cout << "$ ";
+  std::string input;
+  std::getline(std::cin, input);
 }
