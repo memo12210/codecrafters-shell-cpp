f94f9a8b413846bcf09679d69e2b468a2de8a907
Mehmet <mehmetcalikoglufb@gmail.com>
2024-06-08 16:07:54 +0300

    1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,7 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  std::cout << "Logs from your program will appear here!\n";
-
+  
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
