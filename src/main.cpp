394b85cd9693c358d0fb6de7644770a04e539443
Mehmet <mehmetcalikoglufb@gmail.com>
2024-06-08 16:35:17 +0300

    pass 1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main() {
+enum commands
+{
+  echo,
+  cd,
+  invalid
+};
+
+commands string_to_command(std::string str)
+{
+  if(str == "echo") return echo;
+  if(str == "cd") return cd;
+
+  return invalid;
+}
+
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -9,4 +25,8 @@ int main() {
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
+
+  if(string_to_command(input) == invalid)
+    std::cout << input << ": command not found" << '\n';
+  
 }
